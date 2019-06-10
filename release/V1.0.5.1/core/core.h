82206dbd43fc2ac11d76d27e44fffc90326e6e20
coder0&1 <zijian0818@163.com>
2019-06-10 15:36:06 +0800

    del release dir

--- release/V1.0.5.1/core/core.h
+++
@@ -1,9 +0,0 @@
-#ifndef CORE_H
-#define CORE_H
-#include <QtCore>
-#include "database/staticdatabase.h"
-#include "database/realtimedatabase.h"
-bool get_info_from_ab1(FileTable &);
-bool get_info_from_gssp(GsspFileTable &);
-
-#endif // CORE_H
